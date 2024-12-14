b739422dcb6847aa475b50da9cc261152b40d50b
Бахарев Алексей Владиславович <AVlaBakharev@sberbank.ru>
2024-12-14 14:18:05 +0700

    initial commit

---
+++ src/main/java/com/example/qaframework/QaFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.qaframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QaFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/qaframework/QaFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qaframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
